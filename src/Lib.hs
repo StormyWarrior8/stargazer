1e2841a61761d6d606003d764c1444e0bc740064
Eric Bailey <eric@ericb.me>
2016-07-05 05:47:02 -0500

    Add GET /stars handler

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,13 +2,20 @@ module Lib
     ( getStars
     ) where
 
-import           Data.Bool          (bool)
-import           Data.String
-import           Network.GitHub
-import           Servant.Client (ServantError)
-import           System.Environment
+import           Control.Monad.Except   (throwError)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Bool              (bool)
+import           Data.String            (fromString)
+import           Network.GitHub         (Star, recurseOff, recurseOn, runGitHub,
+                                         userStars)
+import           Servant.Server         (Handler, err500)
+import           System.Environment     (lookupEnv)
 
-getStars :: Bool -> IO (Either ServantError [Star])
-getStars shouldRecurse =
-  fmap fromString <$> lookupEnv "GITHUB_TOKEN" >>= runGitHub go
-    where go = bool recurseOff recurseOn shouldRecurse >> userStars
+getStars :: Bool -> Handler [Star]
+getStars shouldRecurse = do
+  token  <- liftIO (fmap fromString <$> (lookupEnv "GITHUB_TOKEN"))
+  let go = bool recurseOff recurseOn shouldRecurse >> userStars
+  result <- liftIO (runGitHub go token)
+  case result of
+    Left  err   -> throwError err500
+    Right stars -> return stars
