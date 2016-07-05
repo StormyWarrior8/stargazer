1c7a04b864ae91e9abf86325088f09eaddc8b135
Eric Bailey <eric@ericb.me>
2016-07-05 05:54:49 -0500

    Add minimal web app

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,41 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeOperators              #-}
+
 module Main where
 
-import Lib
+import           Lib
+import           Network.GitHub.Types
+import           Network.Wai.Handler.Warp
+import           Servant
+
+type API
+  -- GET /stars
+  = "stars" :> Get '[JSON] [Star]
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server
+  -- GET /stars
+  = getStars False
 
 main :: IO ()
 main = do
+  let port = 3000
+  putStrLn $ "GET http://localhost:" ++ (show port) ++ "/stars"
+  run 3000 (serve api server)
+
+{-
   result <- getStars False
   -- result <- fmap head <$> getStars False
   case result of
     Left  err   -> print err
     Right stars -> print stars
+-}
